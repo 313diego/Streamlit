9c5993c5917a50741659722ba57d8219f663ff47
313diego <diego_313@msn.com>
2025-03-31 20:38:20 +0200

    issues

--- Tratamiento Imagenes - alumnado/Streamlit/Documentación.py
+++ Tratamiento Imagenes - alumnado/Streamlit/Documentación.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 from PIL import Image
-import cv2
 import pickle
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
